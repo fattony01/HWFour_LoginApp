12f1523366bd0e7d57c2adc4c607a0af81710c57
fattony01 <a.kondratenko.dev@gmail.com>
2023-06-06 23:49:09 +0200

    all first screen is done without LogIn button func

--- HWFour_LoginApp/LoginViewController.swift
+++ HWFour_LoginApp/LoginViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+//MARK: - IBOutlets
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    
+//MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        userNameTF.autocorrectionType = .no
+        userNameTF.spellCheckingType = .no
+        
+        passwordTF.autocorrectionType = .no
+        passwordTF.spellCheckingType = .no
+        passwordTF.isSecureTextEntry = true
+        
     }
-
+    
+//MARK: - IBActions
+    @IBAction func logInButtonDidTapped() {
+        
+    }
+    
+    @IBAction func forgotUserNameDidTapped() {
+        let alert = UIAlertController(title: "Oops!", message: "Your name is User", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func forgotPasswordDidTapped() {
+        let alert = UIAlertController(title: "Oops!", message: "Your password is Password", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                present(alert, animated: true, completion: nil)
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super .touchesBegan(touches, with: event)
+    }
 
 }
 
